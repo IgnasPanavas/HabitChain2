49a8914986f386f8409603d734d6ee7b26b32ad7
Ignas <ignaspanava@gmail.com>
2024-01-31 12:38:15 -0500

    Adding to GitHub

--- Chains/Persistence/Persistence.swift
+++ Chains/Persistence/Persistence.swift
@@ -6,51 +6,55 @@
 //
 
 import CoreData
+import SwiftUI
 
 struct PersistenceController {
     static let shared = PersistenceController()
-
-    static var preview: PersistenceController = {
-        let result = PersistenceController(inMemory: true)
-        let viewContext = result.container.viewContext
-        for _ in 0..<10 {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-        }
+    
+    func save(context: NSManagedObjectContext) {
         do {
-            try viewContext.save()
-        } catch {
-            // Replace this implementation with code to handle the error appropriately.
-            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            let nsError = error as NSError
-            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            try context.save()
+            print("Content saved successfully")
+        } catch let error as NSError {
+                print("Error Domain: \(error.domain)")
+                print("Error Code: \(error.code)")
+                print("Error Description: \(error.localizedDescription)")
+                print("Error User Info: \(error.userInfo)")
+            
         }
-        return result
-    }()
-
-    let container: NSPersistentContainer
-
-    init(inMemory: Bool = false) {
-        container = NSPersistentContainer(name: "Chains")
-        if inMemory {
-            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
-        }
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-
-                /*
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-        })
-        container.viewContext.automaticallyMergesChangesFromParent = true
     }
-}
+    
+    func addHabitChain(name: String, context: NSManagedObjectContext) {
+        let HabitChainEntity = HabitChain(context: context)
+        HabitChainEntity.id = UUID()
+        HabitChainEntity.name = name
+        HabitChainEntity.isSelected = false
+        save(context: context)
+    }
+    
+    func addHabit(name: String, desc: String, time: Date, r: Float, g: Float, b: Float, habitChain: HabitChain) {
+        
+        let context = habitChain.managedObjectContext
+        
+        let HabitEntity = Habit(context: context!)
+        HabitEntity.name = name
+        HabitEntity.desc = desc
+        HabitEntity.time = Date.now
+        HabitEntity.id = UUID()
+        HabitEntity.r = r
+        HabitEntity.g = g
+        HabitEntity.b = b
+        habitChain.addToHabits(HabitEntity)
+        save(context: context!)
+        
+    }
+    
+    func createSeedData(context: NSManagedObjectContext) {
+        let habitChain1: HabitChain = addHabitChain(name: "Morning", context: context)
+        
+        addHabit(name: "Drink water", desc: "Drink lots of water", time: Date.now, r: 0.33, g: 0.62, b: 0.44, habitChain: habitChain1)
+        
+    }
+    
+        //container.viewContext.automaticallyMergesChangesFromParent = true
+    }
