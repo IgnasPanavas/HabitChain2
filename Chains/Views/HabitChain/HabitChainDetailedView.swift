129f7a4caeabefb4eca63da3a6f4f0e4a19a6223
Ignas <ignaspanava@gmail.com>
2024-02-01 19:34:27 -0500

    Added files

---
+++ Chains/Views/HabitChain/HabitChainDetailedView.swift
@@ -0,0 +1,8 @@
+//
+//  HabitChainDetailedView.swift
+//  Chains
+//
+//  Created by Ignas Panavas on 2/1/24.
+//
+
+import Foundation
---
+++ Chains/Views/HabitChain/HabitChainTitleCardView.swift
@@ -0,0 +1,8 @@
+//
+//  HabitChainTitleCardView.swift
+//  Chains
+//
+//  Created by Ignas Panavas on 2/1/24.
+//
+
+import Foundation
